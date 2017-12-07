4499837c4252b7f6287e0476acb6528103db8d17
blrzefir <aternovski@gmail.com>
2017-12-07 11:49:19 +0300

    aternovski a,b,c(1,2) complete

---
+++ src/by/it/aternovski/lesson02/TaskA2.java
@@ -0,0 +1,31 @@
+package by.it.aternovski.lesson02;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+class TaskA2 {
+    public static void main(String[] args) {
+  /*      for (int i = 1; i == 5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+    }
+}
+/*
+      /*  for (i = 1; i == 5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+    }
+}*/
+
+   System.out.println ("Я начинаю изучать Java!");
+    System.out.println ("Я начинаю изучать Java!");
+    System.out.println ("Я начинаю изучать Java!");
+    System.out.println ("Я начинаю изучать Java!");
+    System.out.println ("Я начинаю изучать Java!");
+
+}
+}
---
+++ src/by/it/aternovski/lesson02/TaskC2.java
@@ -0,0 +1,34 @@
+package by.it.aternovski.lesson02;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и 4 раза выводит их сумму с обозначением системы счисления на экран в
+ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
+
+Ввод:
+34 26
+
+Вывод:
+DEC:34+26=60
+BIN:100010+11010=111100
+HEX:22+1a=3c
+OKT:42+32=74
+*/
+
+import java.util.Scanner;
+class TaskC2 {
+    public static void main (String [] args){
+        System.out.println("Введите пжлста два числа от 1 до 99");
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = n1 + n2;
+        System.out.println("Вывод: ");
+        System.out.println("DEC: "+n1+" "+"+"+" "+n2+" "+"="+" "+(n1+n2) );
+        System.out.println("BIN: "+(Integer.toString(n1, 2))+" "+"+"+" "+(Integer.toString(n2, 2)) +" "+"="+" "+(Integer.toString(n3,2)));
+        System.out.println("HEX: "+(Integer.toString(n1, 8))+" "+"+"+" "+(Integer.toString(n2, 8)) +" "+"="+" "+(Integer.toString(n3,8)));
+        System.out.println("OKT: "+(Integer.toString(n1, 16))+" "+"+"+" "+(Integer.toString(n2, 16)) +" "+"="+" "+(Integer.toString(n3,16)));
+    //+(Integer.toString(n2, 2))))
+    }
+//System.out.println(Integer.toString(a, 2));
+
+}
