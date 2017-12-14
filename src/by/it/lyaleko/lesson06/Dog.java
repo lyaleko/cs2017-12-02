874610e10a3b730af1fa79f28f0327b32dc50795
lyaleko <M_hodanenok@mail.ru>
2017-12-14 21:59:18 +0300

    lesson 2

---
+++ src/by/it/lyaleko/lesson06/Dog.java
@@ -0,0 +1,39 @@
+package by.it.lyaleko.lesson06;
+
+public class Dog {
+
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Кличка: %s. Возраст: %d", name, age);
+    }
+
+    public Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog() {
+
+
+    }
+
+}
